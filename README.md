3b551dcd46aed3d3723376fe31651df5098d5355
Em <emily.m.a.marin@gmail.com>
2020-12-10 18:56:28 -0500

    Modify README description and add python analysis file

--- README.md
+++ README.md
@@ -1,1 +1,75 @@
-# tik_tok_projects
+# Em's Tik Tok Projects lol
+
+Are you here from Tik Tok?
+**PLEASE** read this before jumping in!
+
+The files in this repo are light-hearted and casually made in my free time. If you're interested in making any changes, feel free to DM me and make a pull request.
+
+The code presented here in this repository is made with good intentions and for recreational fun, it is not meant to:
+1. Be used against others.
+2. Be used to exploit others.
+3. Be used to illicit dangerous, harmful, or destructive behavior in any manner.
+
+Basically, lets have fun and be a good people :)
+To read more about ethical dependencies, check out Wampum.codes' article on using indigenous wisdom in software development and design
+[here](https://foundation.mozilla.org/en/blog/indigenous-wisdom-model-software-design-and-development/)
+
+
+## Hinge Analysis
+
+So you wanna learn more about about your Hinge habits? Look no further!
+
+To request your data, follow the steps [here on Hinge's site](https://hingeapp.zendesk.com/hc/en-us/articles/360011235813-How-do-I-request-a-copy-of-my-personal-data-)
+
+If you do not have a background in programming and would like to plug your data and go, *a website for that is coming soon!*
+
+If you're interested in running this python script yourself, continue reading.
+
+### Prerequisites
+
+Before running, make sure you've got [Python](https://www.python.org/downloads/) set up in your development environment.
+
+### Installation
+```
+# Option 1: Copy paste the contents of lib/analyze.py into a python file located in the same directory as your Hinge matches.json file
+
+# Option 2: Cloning the repository
+$ git clone git://github.com/emam97/tiktok.git
+```
+
+### Usage
+```
+python analyze.py matches.json
+
+```
+
+### Examples
+Given your python file is `analyze.py` and the file you received from Hinge is `matches.json` in the directory 
+`C:/User/em/hinge_data`
+```
+$ C:/User/em/hinge_data> python analyze.py matches.json
+Parsing data...
+Analysis complete!
+
+You've interacted with 100 people on Hinge
+
+Total shots you've shot: 45 (you sent a like)
+Total slam dunks: 61 (you've matched with someone or they've matched with you)
+Total people you've blessed with your time: 41 (you sent a message at least once)
+-------------
+
+
+Fun Stuff ~
+
+You've rejected 39 people (ouch)
+You match with 61% of profiles you see on Hinge.
+
+You've ghosted at least 20 people, or at least 30% of all the people you match.
+Total you've dropped the ball: 10 (you sent a like and they matched, no chatting)
+Total they've dropped the ball: 10 (they sent a like and you matched, no chatting)
+
+Total shots they've shot and scored: 31 (they sent a like and you matched)
+Total shots you've shot and scored: 30 (you sent a like and they matched)
+```
+
+
---
+++ lib/analyze.py
@@ -0,0 +1,69 @@
+import json
+import sys
+
+input_file = sys.argv[1]
+
+print("Parsing data...")
+
+rejected = 0
+likes = 0
+matches = 0
+chats = 0
+u_ghosted = 0
+unrequited_love = 0
+they_dunk = 0
+they_ghosted = 0
+u_dunk = 0
+total_interactions = 0
+
+with open(input_file, 'r') as f:
+    data = json.load(f)
+    for blob in data:
+        interactions = list(blob.keys())
+        first_interaction = interactions[0]
+        if len(interactions) > 1:
+            second_interaction = interactions[1]
+        else:
+            second_interaction = 'noop'
+        total_interactions += 1
+        if first_interaction == 'block':
+            rejected += 1
+        elif first_interaction == 'like':
+            likes += 1
+            if second_interaction == 'match':
+                matches += 1
+                u_ghosted += 1
+                u_dunk +=1
+        elif first_interaction == 'match':
+            matches += 1
+            they_dunk += 1
+            they_ghosted += 1
+        elif first_interaction == 'chats':
+            chats += 1
+            if second_interaction == 'like':
+                likes += 1
+                matches += 1
+                u_dunk +=1
+            elif second_interaction == 'match':
+                matches += 1
+                they_dunk += 1
+
+tot_string = "You've interacted with " + str(total_interactions) + " people on Hinge \n"
+rej_string = "You've rejected " + str(rejected) + " people (ouch) \nYou match with " + str(matches/total_interactions * 100) + "% of profiles you see on Hinge.\n"
+tot_ghost_string = "You've ghosted at least " + str(u_ghosted + they_ghosted) + ", or at least " + str((u_ghosted+they_ghosted)/matches * 100) + "% of all the people you match with\n"
+u_ghost_string = "Total you've dropped the ball: " + str(u_ghosted) + " (you sent a like and they matched, no chatting) \n"
+they_ghost_string = "Total they've dropped the ball " + str(they_ghosted) + " (they sent a like and you matched, no chatting) \n"
+they_dunk_string = "Total shots they've shot and scored: " + str(they_dunk) + " (they sent a like and you matched) \n"
+u_dunk_string = "Total shots you've shot and scored: " + str(u_dunk) + " (you sent a like and they matched) \n"
+tot_likes = "Total shots you've shot: " + str(likes) + " (you sent a like)\n"
+tot_matches = "Total slam dunks: " + str(matches) + " (you've matched with someone or they've matched with you) \n"
+tot_chats = "Total people you've blessed with your time: " + str(chats) + " (you sent a message at least once) \n"
+
+analysis = tot_string + tot_likes + tot_matches + tot_chats
+analysis += "\n------------- \n\n\n"
+analysis += "Fun Stuff ~ \n\n"
+analysis += rej_string + tot_ghost_string + u_ghost_string + they_ghost_string + they_dunk_string + u_dunk_string
+
+
+print("Analysis complete!")
+print(analysis)
